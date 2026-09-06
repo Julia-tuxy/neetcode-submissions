9b1efebba173c489347fdc9c11d504edd86cdcea
Julia-tuxy <97815103+Julia-tuxy@users.noreply.github.com>
2026-09-06 15:23:50 -0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = {} # key: [val, stamps]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        storeRes = self.store.get(key, [])
+
+        l, r = 0, len(storeRes)- 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if storeRes[m][1] <= timestamp:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return storeRes[r][0] if r >=0 else ""
